28d778db0115163e049ce12a2397daca9ce61ecd
Tobias Thy <thymusiker@gmail.com>
2024-01-09 10:40:14 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,280 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.warningLabel = new System.Windows.Forms.Label();
+            this.warningLabel2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.table = new System.Windows.Forms.DataGridView();
+            this.label5 = new System.Windows.Forms.Label();
+            this.table2 = new System.Windows.Forms.DataGridView();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.panel1.Controls.Add(this.label9);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label8);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label7);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label6);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(267, 598);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(122, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Luftfugtighed:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 76);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(113, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Temperatur: ";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lucida Sans Unicode", 18F, System.Drawing.FontStyle.Bold);
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(182, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Lageroversigt";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(901, -71);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(204, 220);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // warningLabel
+            // 
+            this.warningLabel.AutoSize = true;
+            this.warningLabel.Font = new System.Drawing.Font("Lucida Sans Unicode", 13F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.warningLabel.ForeColor = System.Drawing.Color.Red;
+            this.warningLabel.Location = new System.Drawing.Point(273, 9);
+            this.warningLabel.Name = "warningLabel";
+            this.warningLabel.Size = new System.Drawing.Size(103, 22);
+            this.warningLabel.TabIndex = 0;
+            this.warningLabel.Text = "(advarsel)";
+            this.warningLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.warningLabel.Visible = false;
+            // 
+            // warningLabel2
+            // 
+            this.warningLabel2.AutoSize = true;
+            this.warningLabel2.Font = new System.Drawing.Font("Lucida Sans Unicode", 13F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))));
+            this.warningLabel2.ForeColor = System.Drawing.Color.Red;
+            this.warningLabel2.Location = new System.Drawing.Point(273, 41);
+            this.warningLabel2.Name = "warningLabel2";
+            this.warningLabel2.Size = new System.Drawing.Size(103, 22);
+            this.warningLabel2.TabIndex = 2;
+            this.warningLabel2.Text = "(advarsel)";
+            this.warningLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.warningLabel2.Visible = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Lucida Sans Unicode", 15F, System.Drawing.FontStyle.Bold);
+            this.label4.Location = new System.Drawing.Point(273, 178);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(174, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Overskudslager";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // table
+            // 
+            this.table.AllowUserToAddRows = false;
+            this.table.AllowUserToDeleteRows = false;
+            this.table.AllowUserToResizeColumns = false;
+            this.table.AllowUserToResizeRows = false;
+            this.table.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.table.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.table.GridColor = System.Drawing.Color.Black;
+            this.table.Location = new System.Drawing.Point(273, 204);
+            this.table.Name = "table";
+            this.table.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Sunken;
+            this.table.Size = new System.Drawing.Size(379, 382);
+            this.table.TabIndex = 5;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Lucida Sans Unicode", 15F, System.Drawing.FontStyle.Bold);
+            this.label5.Location = new System.Drawing.Point(654, 178);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 23);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Mangellager";
+            // 
+            // table2
+            // 
+            this.table2.AllowUserToAddRows = false;
+            this.table2.AllowUserToDeleteRows = false;
+            this.table2.AllowUserToResizeColumns = false;
+            this.table2.AllowUserToResizeRows = false;
+            this.table2.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(25)))), ((int)(((byte)(25)))));
+            this.table2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.table2.GridColor = System.Drawing.Color.Black;
+            this.table2.Location = new System.Drawing.Point(658, 204);
+            this.table2.Name = "table2";
+            this.table2.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.Sunken;
+            this.table2.Size = new System.Drawing.Size(435, 382);
+            this.table2.TabIndex = 7;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Lucida Sans Unicode", 15F, System.Drawing.FontStyle.Bold);
+            this.label6.Location = new System.Drawing.Point(12, 178);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(115, 23);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Tidszoner";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(12, 223);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(91, 20);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Tidszone1";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(12, 281);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(91, 20);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Tidszone2";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Lucida Sans Unicode", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(12, 342);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(91, 20);
+            this.label9.TabIndex = 11;
+            this.label9.Text = "Tidszone3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(60)))), ((int)(((byte)(60)))), ((int)(((byte)(60)))));
+            this.ClientSize = new System.Drawing.Size(1105, 598);
+            this.Controls.Add(this.table2);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.table);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.warningLabel2);
+            this.Controls.Add(this.warningLabel);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.panel1);
+            this.ForeColor = System.Drawing.Color.White;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "WineHub";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.table2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label warningLabel;
+        private System.Windows.Forms.Label warningLabel2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DataGridView table;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DataGridView table2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,234 @@
+﻿using DVI_Access_Lib;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        //Set variables with API data
+        private DVI_Climate climate = new DVI_Climate("http://docker.data.techcollege.dk:5051");
+        private DVI_Stock stock = new DVI_Stock("http://docker.data.techcollege.dk:5051");
+
+        //generate the window, and components
+        public Form1()
+        {
+            //Initialize components
+            InitializeComponent();
+
+            //Start while-loops
+            GetStockAsync();
+            GetClimateAsync();
+            GetTime();
+        }
+
+        //Get Timezones and refresh every second
+        private async void GetTime()
+        {
+            //Start while-loop that gets the time, and sets them to labels in the UI
+            while (true)
+            {
+                //set the local timezone
+                var localZone = await Task.Run(() => DateTime.Now.ToString("hh:mm:ss"));
+
+                // Get time for Washington
+                var easternZone = TimeZoneInfo.FindSystemTimeZoneById("Eastern Standard Time");
+                var washingtonTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, easternZone).ToString("hh:mm:ss");
+
+                // Get time for Beijing
+                var chinaZone = TimeZoneInfo.FindSystemTimeZoneById("China Standard Time");
+                var beijingTime = TimeZoneInfo.ConvertTimeFromUtc(DateTime.UtcNow, chinaZone).ToString("hh:mm:ss");
+                //Start the action after data has been received
+                label7.Invoke(new Action(() =>
+                {
+                    label7.Text = "København \n" + localZone;
+                    label8.Text = "Washington DC \n" + washingtonTime;
+                    label9.Text = "Beijing \n" + beijingTime;
+                }));
+                //wait 1 second, then run again
+                await Task.Delay(1000);
+            }
+        }
+        //Get stock info, and get the needed items
+        private async void GetStockAsync()
+        {
+            //set up the "table" dataGridView context header
+            table.Columns.Add("numInStock", "Antal");
+            table.Columns.Add("wineName", "Navn");
+            //set The width of table
+            table.Columns[0].Width = 50;
+            table.Columns[1].Width = 280;
+            table.ForeColor = Color.Black;
+
+            //set up the "table2" dataGridView context header
+            table2.Columns.Add("numInStock", "Antal");
+            table2.Columns.Add("wineName", "Navn");
+            //Set the width of table2
+            table2.Columns[0].Width = 50;
+            table2.Columns[1].Width = 280;
+            table2.ForeColor = Color.Black;
+
+            //Start the while loop
+            while (true)
+            {
+                //clear old table content
+                table.Rows.Clear();
+                //set the StockOverThreshold as used variable
+                var winesOver = await Task.Run(() => stock.StockOverThreshold());
+                //Put every item in "winesOver" into the table
+                foreach (Wine wine in winesOver)
+                {
+                    table.Rows.Add(wine.NumInStock.ToString(), wine.WineName.ToString());
+                }
+
+                //clear old table content
+                table2.Rows.Clear();
+                //set StockOverThreshold as used variable
+                var winesUnder = await Task.Run(() => stock.StockUnderThreshold());
+                //Put every item in "wineUnder" into the table
+                foreach (Wine wine in winesUnder)
+                {
+                    table2.Rows.Add(wine.NumInStock.ToString(), wine.WineName.ToString());
+                }
+                //Wait 30 seconds, then run again
+                await Task.Delay(30000);
+            }
+        }
+
+        //Get and set climate data and refresh every 5 seconds
+        private async void GetClimateAsync()
+        {
+            //set the loop
+            while (true)
+            {
+                //get the API values once, invoke the action in which the value is used
+                float temp = await Task.Run(() => climate.CurrTemp());
+                float maxTemp = await Task.Run(() => climate.MaxTemp());
+                float minTemp = await Task.Run(() => climate.MinTemp());
+                //start the action when the value has been received
+                label2.Invoke(new Action(() =>
+                {
+                    //if the temperature value is above the max, then run the warning
+                    if (temp > maxTemp)
+                    {
+                        //alarm sound
+                        Console.Beep();
+                        //set color of the temperature label to red
+                        label2.ForeColor = Color.DarkRed;
+                        //put the value in the label
+                        label2.Text = "Temperatur: " + temp.ToString("0.00") + "°";
+                        //set the warning label text, and make it visible
+                        warningLabel.Text = "Advarsel! Temperatur på lager er for høj.";
+                        warningLabel.Visible = true;
+                    }
+                    // - or if the temperature is below the max, then run warning
+                    else if (temp < minTemp)
+                    {
+                        //alarm sound
+                        Console.Beep();
+                        //set the color of temperature label to red
+                        label2.ForeColor = Color.DarkRed;
+                        //put the value in the label
+                        label2.Text = "Temperatur: " + temp.ToString("0.00") + "°";
+                        //set the warning label text, and make it visible
+                        warningLabel.Text = "Advarsel! Temperatur på lager er for lav.";
+                        warningLabel.Visible = true;
+                    }
+                    //if the warning conditions havent been met, then display temperature like normal
+                    else
+                    {
+                        //set color to green
+                        label2.ForeColor = Color.Green;
+                        //set the label text and value
+                        label2.Text = "Temperatur: " + temp.ToString("0.00") + "°";
+                        //hide the warning label
+                        warningLabel.Visible = false;
+                    }
+                }));
+
+                //get the API values once, invoke the action in which the value is used
+                float hum = await Task.Run(() => climate.CurrHum());
+                float maxHum = await Task.Run(() => climate.MaxHum());
+                float minHum = await Task.Run(() => climate.MinHum());
+                //Start action when the value is received
+                label3.Invoke(new Action(() =>
+                {
+                    //if the humidity value is above the max, then run the warning
+                    if (hum > maxHum)
+                    {
+                        Console.Beep();
+                        label3.ForeColor = Color.DarkRed;
+                        label3.Text = "Luftfugtighed: " + hum.ToString("0.00") + "%";
+                        warningLabel2.Text = "Advarsel! ´Luftfugtighed på lager er for høj.";
+                        warningLabel2.Visible = true;
+                    }
+                    // - or if the humidity value is below the max, then run the warning
+                    else if (hum < minHum)
+                    {
+                        Console.Beep();
+                        label3.ForeColor = Color.DarkRed;
+                        label3.Text = "Luftfugtighed: " + hum.ToString("0.00") + "%";
+                        warningLabel2.Text = "Advarsel! Luftfugtighed på lager er for lav.";
+                        warningLabel2.Visible = true;
+                    }
+                    //if the warning conditions havent been met, then display temperature like normal
+                    else
+                    {
+                        label3.ForeColor = Color.Green;
+                        label3.Text = "Luftfugtighed: " + hum.ToString("0.00") + "%";
+                        warningLabel2.Visible = false;
+                    }
+                }));
+                //Wait 5 seconds, then run again
+                await Task.Delay(5000);
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
